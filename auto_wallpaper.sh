3b8f5b9439ccac7c65265fd359e2bf302c713872
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-08-11 09:27:21 +0200

    Update auto_wallpaper.sh

--- auto_wallpaper.sh
+++ auto_wallpaper.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-auto_set_bing_wallpaper/url_wallpaper.py > url
+cd /home/tmendy/my_scripts/auto_set_bing_wallpaper/
+./url_wallpaper.py > url
 while read line; do
 	echo $line
 	curl $line --output wallpaper.jpg; 
 done < url
-rm url
-
+sudo rm url
 sudo mv wallpaper.jpg /usr/share/wallpapers/bing_wallpaper.jpg
+cd -
 
 feh --bg-scale --zoom fill /usr/share/wallpapers/bing_wallpaper.jpg
