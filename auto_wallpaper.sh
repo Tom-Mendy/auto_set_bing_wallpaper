7f83e8fcd170cedd4bbe4efbf2dc5834baf9c351
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-02 14:38:36 +0200

    UPD ALGO ONLY BASH

--- auto_wallpaper.sh
+++ auto_wallpaper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd /home/tmendy/my_scripts/auto_set_bing_wallpaper/
-./url_wallpaper.py > url
-while read line; do
-	echo $line
-	curl $line --output wallpaper.jpg; 
-done < url
+wget "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=%sen_US" -O xml_page
+
+base_url="https://www.bing.com"
+url=$(cat xml_page |cut --delimiter="<" --fields=11 |cut --character=5-)
+url="$base_url$url"
+rm -f xml_page
+
+echo $line
+curl $line --output wallpaper.jpg; 
 mv wallpaper.jpg /home/tmendy/.bing_wallpaper.jpg
-cd $OLDPWD
 
 feh --bg-scale --zoom fill /home/tmendy/.bing_wallpaper.jpg
