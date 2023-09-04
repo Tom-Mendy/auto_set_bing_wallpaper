739fa57398d73e81068acab108e4e3ae2ca6ec55
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-04 09:39:45 +0200

    FIX SCRIPT

--- auto_wallpaper.sh
+++ auto_wallpaper.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#get image url
 wget "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=%sen_US" -O xml_page
-
 base_url="https://www.bing.com"
 url=$(cat xml_page |cut --delimiter="<" --fields=11 |cut --character=5-)
 url="$base_url$url"
 rm -f xml_page
 
-echo $line
-curl $line --output wallpaper.jpg; 
+#dowload image
+echo $url
+curl $url --output wallpaper.jpg; 
 mv wallpaper.jpg /home/tmendy/.bing_wallpaper.jpg
 
+#use feh to set image as wallpaper
 feh --bg-scale --zoom fill /home/tmendy/.bing_wallpaper.jpg
