6989d07353d14d743cbb35663a7d0370a16c22a0
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2024-02-10 11:17:21 +0100

    remove wget to only use curl

--- auto_wallpaper.sh
+++ auto_wallpaper.sh
@@ -2,7 +2,7 @@
 
 generate_file() {
     #get image url
-    wget "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=%sen_US" -O xml_page
+    curl "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=%sen_US" --output xml_page
     base_url="https://www.bing.com"
     url=$(cat xml_page | cut --delimiter="<" --fields=11 |cut --character=5-)
     url="$base_url$url"
